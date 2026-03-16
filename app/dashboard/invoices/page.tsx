c500b14233f31edb100820333885f53c9b15be51
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2026-03-16 10:10:55 -0400

    fix: add Suspense around Search component to prevent Vercel runtime error

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -13,7 +13,15 @@ export default async function Page() {
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Search invoices..." />
+        {/*
+          NOTE: Suspense is not working with search params, so we need to use a fallback .
+          This addition was not in the tutorial. And resolves a Vercel runtime error, while
+          in development of this tutorial. Perhaps a change later in the tutorial addresses
+          this issue.
+        */}
+        <Suspense fallback={ <div className="h-10 w-full max-w-sm rounded-md bg-gray-100" /> } >
+          <Search placeholder="Search invoices..." />
+        </Suspense>
         <CreateInvoice />
       </div>
       {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
