cf2400b444f8e8b465478d5f7fe833f0891adafe
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2026-03-27 12:29:27 -0400

    feat: add edit invoice functionality

---
+++ src/app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,29 @@
+import EditInvoiceForm from '@/components/invoices/EditInvoiceForm';
+import Breadcrumbs from '@/components/invoices/Breadcrumbs';
+import { fetchCustomers, fetchInvoiceById } from '@/lib/data';
+
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const id = params.id;
+
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <EditInvoiceForm invoice={invoice} customers={customers} />
+    </main>
+  );
+}
